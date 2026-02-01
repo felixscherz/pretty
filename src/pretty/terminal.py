18aa90e78bf5243baf649e5b527760187879eb03
Felix Scherz <felixwscherz@gmail.com>
2026-02-01 13:53:52 +0100

    feat: (somewhat) working example

--- src/pretty/terminal.py
+++ src/pretty/terminal.py
@@ -1,8 +1,8 @@
 import pty
 import os
-import time
+import select
 import sys
-import tkinter
+import threading
 from typing import assert_never
 
 # 1. get a pty 
@@ -22,16 +22,39 @@ def main():
         os.dup2(client, 1) # stdout
         os.dup2(client, 2) # stderr
         os.execv("/bin/zsh", ["/bin/zsh"])
-        time.sleep(5)
         assert_never()
 
     # main process
 
 
+    def read_from_stdin():
+        while True:
+            r, _, _ = select.select([sys.stdin.fileno()], [], [], 0.1)
+            if r:
+                data = os.read(sys.stdin.fileno(), 1024)
+                os.write(controller, data)
 
-    ...
 
-    time.sleep(5)
+    def write_from_controller():
+        while True:
+            r, _, _ = select.select([controller], [], [], 0.1)
+            if r:
+                data = os.read(r[0], 1024)
+                print(data)
+
+
+
+    read_thread = threading.Thread(target=read_from_stdin, daemon=True)
+    write_thread = threading.Thread(target=write_from_controller, daemon=True)
+
+    read_thread.start()
+    write_thread.start()
+
+
+    read_thread.join()
+    write_thread.join()
+
+
 
 if __name__ == "__main__":
     main()
