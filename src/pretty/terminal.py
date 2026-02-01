2ab238a8f4800911e34c552e5de8e1f49ac450a0
Felix Scherz <felixwscherz@gmail.com>
2026-02-01 14:16:09 +0100

    ci: add pre-commit

--- src/pretty/terminal.py
+++ src/pretty/terminal.py
@@ -5,8 +5,9 @@ import sys
 import threading
 from typing import assert_never
 
-# 1. get a pty 
+# 1. get a pty
 # 2. fork the process
+
 
 def main():
     # this opens a pair of file descriptors
@@ -18,9 +19,9 @@ def main():
     if pid == 0:
         # child process
 
-        os.dup2(client, 0) # stdin
-        os.dup2(client, 1) # stdout
-        os.dup2(client, 2) # stderr
+        os.dup2(client, 0)  # stdin
+        os.dup2(client, 1)  # stdout
+        os.dup2(client, 2)  # stderr
         env = os.environ.copy()
         env["PS1"] = "MY-CUSTOM-PROMPT%  "
         os.execve("/bin/zsh", ["/bin/zsh", "-f"], env)
@@ -28,14 +29,12 @@ def main():
 
     # main process
 
-
     def read_from_stdin():
         while True:
             r, _, _ = select.select([sys.stdin.fileno()], [], [], 0.1)
             if r:
                 data = os.read(sys.stdin.fileno(), 1024)
                 os.write(controller, data)
-
 
     def write_from_controller():
         while True:
@@ -47,21 +46,15 @@ def main():
                 sys.stdout.buffer.write(data)
                 sys.stdout.buffer.flush()
 
-
-
     read_thread = threading.Thread(target=read_from_stdin, daemon=True)
     write_thread = threading.Thread(target=write_from_controller, daemon=True)
 
     read_thread.start()
     write_thread.start()
 
-
     read_thread.join()
     write_thread.join()
 
 
-
 if __name__ == "__main__":
     main()
-
-
